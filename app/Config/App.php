31d1eaef79a0c7e2ad2757ae01e67ab9cfaff951
Wempy Aditya Wiryawan <67529099+wempy-aditya@users.noreply.github.com>
2020-07-12 17:04:00 +0700

    update app.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://app.wempy.xyz';
+	public $baseURL = 'https://app.wempy.xyz';
 
 	/*
 	|--------------------------------------------------------------------------
